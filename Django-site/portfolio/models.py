65b2a3e23e8e18db358ef0b20e7ef883a5364c32
Eric Philippot <EricpDev@gmail.com>
2024-09-24 22:26:32 -0500

    Added first Django Model to the project

--- Django-site/portfolio/models.py
+++ Django-site/portfolio/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class MyNotes(models.Model):
+    """
+    Model for creating and storing notes on Django and Python subject matter.
+    Title: String, Date: NewDate, Subject: String, Content: String, Draft: Boolean
+    """
+    title = models.CharField(max_length=50, blank=False)
+    date = models.DateField(auto_now_add=True)
+    subject = models.CharField(max_length=50, blank=False)
+    content = models.TextField()
+    draft = models.BooleanField(default=True)
+
